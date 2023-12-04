a0c068b6b40f4e77bfd6893b5228eef758ad13e7
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-04 15:55:21 +0300

    Первое приложение

--- src/main/java/org/semen/spring/TestBean.java
+++ src/main/java/org/semen/spring/TestBean.java
@@ -1,2 +1,17 @@
-package org.semen.spring;public class TestBean {
+package org.semen.spring;
+
+public class TestBean {
+    String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
