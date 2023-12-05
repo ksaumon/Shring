d31ac018592d92f3d7530680ac2c3937bb01d516
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-05 17:06:17 +0300

    Аннотации @Autowired зависимость через конструктор

--- src/main/java/org/semen/spring/MusicPlayer.java
+++ src/main/java/org/semen/spring/MusicPlayer.java
@@ -1,41 +1,23 @@
 package org.semen.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+    private final ClassicalMusic classicalMusic;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {}
-
-    public void setMusic(Music music) {
-        this.music = music;
+    @Autowired// анатация которая внедряет зависимости использование конструктор,метод,поля, а с полями приват
+    //раьотает через рефликсию
+    public MusicPlayer(ClassicalMusic classicalMusic) {
+        this.classicalMusic = classicalMusic;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalMusic.getSong());
     }
 
 
