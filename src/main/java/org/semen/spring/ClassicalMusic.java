caae781eb08141beb86de4a140ddce736b74e3f8
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-05 16:26:33 +0300

    Аннотации @Component

--- src/main/java/org/semen/spring/ClassicalMusic.java
+++ src/main/java/org/semen/spring/ClassicalMusic.java
@@ -1,22 +1,34 @@
 package org.semen.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component// данная анатация говорит спринг что из класса ClassicalMusic нужно создать бин в скобках можно прописать
+        //айди например @Component("musicBean") или оставить как есть тогда айди будет classicalMusic класс с маленькой
 public class ClassicalMusic implements Music {
-    private ClassicalMusic() {}// для ограничения создания обьектов через new
-
-    public static ClassicalMusic getClassicalMusic() {// создаем фабричный метод который содает обьект
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit() {// вызываеться перед созданием бина название любое
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroy() {// вызываеться перед закрытием приложения название любое
-        System.out.println("Doing my destruction");
-    }
-
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
     }
 }
+
+
+//public class ClassicalMusic implements Music {
+//    private ClassicalMusic() {}// для ограничения создания обьектов через new
+//
+//    public static ClassicalMusic getClassicalMusic() {// создаем фабричный метод который содает обьект
+//        return new ClassicalMusic();
+//    }
+//
+//    public void doMyInit() {// вызываеться перед созданием бина название любое
+//        System.out.println("Doing my initialization");
+//    }
+//
+//    public void doMyDestroy() {// вызываеться перед закрытием приложения название любое
+//        System.out.println("Doing my destruction");
+//    }
+//
+//    @Override
+//    public String getSong() {
+//        return "Hungarian Rhapsody";
+//    }
+//}
--- src/main/java/org/semen/spring/TestSpring.java
+++ src/main/java/org/semen/spring/TestSpring.java
@@ -3,28 +3,47 @@ package org.semen.spring;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
+
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+        Music music = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
 
-//        MusicPlayer ferstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = ferstMusicPlayer == secondMusicPlayer;
-//        System.out.println(comparison);
-//        System.out.println(ferstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        ferstMusicPlayer.setVolume(10);
-//        System.out.println(ferstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
-//
-////        System.out.println(musicPlayer.getName());
-////        System.out.println(musicPlayer.getVolume());
+        Music music1 = context.getBean("popMusic", Music.class);
+        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+        musicPlayer1.playMusic();
+
+        Music music2 = context.getBean("rockMusic", Music.class);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+        musicPlayer2.playMusic();
         context.close();
     }
+
+//    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml");
+//        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+//        System.out.println(classicalMusic.getSong());
+//
+////        MusicPlayer ferstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+////        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+////
+////        boolean comparison = ferstMusicPlayer == secondMusicPlayer;
+////        System.out.println(comparison);
+////        System.out.println(ferstMusicPlayer);
+////        System.out.println(secondMusicPlayer);
+////
+////        ferstMusicPlayer.setVolume(10);
+////        System.out.println(ferstMusicPlayer.getVolume());
+////        System.out.println(secondMusicPlayer.getVolume());
+////
+//////        System.out.println(musicPlayer.getName());
+//////        System.out.println(musicPlayer.getVolume());
+//        context.close();
+//    }
 
 
 //    public static void main(String[] args) {
