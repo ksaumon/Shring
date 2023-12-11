6621b59206837b3e337ea107afdb3a2b48d82c6b
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-11 13:45:02 +0300

    Аннотации @Value

--- src/main/java/org/semen/spring/TestSpring.java
+++ src/main/java/org/semen/spring/TestSpring.java
@@ -7,10 +7,19 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
+
+//    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml");
+//        Computer computer = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+//        context.close();
+//    }
 
 //    public static void main(String[] args) {
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
