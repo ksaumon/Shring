529fee3caa5e2821094d8c998fc379df1122b807
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-14 14:40:09 +0300

    Урок 17: Контроллеры. Аннотация @Controller и @RequestMapping

---
+++ src/main/java/org/semen/spring/controllers/FirstController.java
@@ -0,0 +1,20 @@
+package org.semen.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/first") данная анатация перед классом говорит что обращаться нужно не /hello а /first/hello
+// и /first/goodbye
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org" lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <title>My app</title>
+</head>
+
+<body>
+
+<p>Home screen!)</p>
+
+<a href="/hello">Say hello</a> or <a href="/goodbye">Say goodbye</a> or <a href="/exit">Exit</a>
+</body>
+
+</html>
