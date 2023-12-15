ee45ed89f1cb4c64e7adc7c9d9f267760ae2cdf2
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-15 13:49:52 +0300

    Модель. Передача данных от контроллера к представлению.

--- src/main/java/org/semen/spring/controllers/FirstController.java
+++ src/main/java/org/semen/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.semen.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +21,10 @@ public class FirstController {
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(@RequestParam("name") String name,
-                              @RequestParam("surname") String surname) {// при данной обработки пришедших
-        //с гет запросом параметров нужно прописыват URL не просто /goodbye а /goodbye?name=Jon&surname=Tomson
-        // иначе будет ошибка или написать @RequestParam("name", required = false) String name,
-        //                              @RequestParam("surname", required = false) String surname ошибки не будет.
-        System.out.println("Hello " + name + " " + surname);
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname, Model model) {
+        //System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/goodbye";
     }
 }
