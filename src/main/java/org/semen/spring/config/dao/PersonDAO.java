488a5128d2e9046b58a9578873d66261c73e9a76
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-12-22 15:19:52 +0300

    SQL инъекции. PreparedStatement. JDBC API.

--- src/main/java/org/semen/spring/config/dao/PersonDAO.java
+++ src/main/java/org/semen/spring/config/dao/PersonDAO.java
@@ -52,25 +52,56 @@ public class PersonDAO {
     }
 
     public Person show(int id){
-       // return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
 
     public void save(Person person) {
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge()
-                    + ",'" + person.getEmail() + "')";// данный способ неудобен и так делать ненужно опасен большим
-            // количеством ошибок и sql инукциями
-            statement.executeUpdate(SQL);
+            PreparedStatement preparedStatement = connection.prepareStatement
+                    ("INSERT INTO Person VALUES(1, ?, ?, ?)");// данный способ более безопасен и удобен и убережет
+            // от SQL иньекций
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+//            Statement statement = connection.createStatement();
+//            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge()
+//                    + ",'" + person.getEmail() + "')";// данный способ неудобен и так делать ненужно опасен большим
+//            // количеством ошибок и sql инукциями
+            // statement.executeUpdate(SQL);
+            preparedStatement.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
     public void update(int id, Person updatedPerson) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
 //        Person personToBeUpdated = show(id);
 //        personToBeUpdated.setName(updatedPerson.getName());
 //        personToBeUpdated.setAge(updatedPerson.getAge());
@@ -78,6 +109,13 @@ public class PersonDAO {
     }
 
     public void delete(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 //        people.removeIf(p -> p.getId() == id);
     }
 
